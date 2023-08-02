bd230abb08245e99c9087ce8b040094dfb881b1c
Themba Mtshelwane <Thembamm3@gmail.com>
2023-08-02 20:27:23 +0200

    Home page- FE-1: JS grid redo and submit logic

--- src/modules/keyInputs.js
+++ src/modules/keyInputs.js
@@ -1,9 +1,30 @@
 const keyboardClicked = document.querySelectorAll('.keyinput') // Allow click to be activated on virtual keyboard
 const keyboardPressed = document.querySelector('body') // Allow press keys to be activated anywhere on the page
-const grid = document.querySelectorAll('.cell') // Get all the cells of the grid
+const gridFromHTML = document.querySelectorAll('.cell') // Get all the cells of the grid
+
 const MAX_NUMBER_OF_LETTERS =5
 const MAX_NUMBER_OF_TRIES =6
-let letter_counter =0 
+let letter_postion =0 
+let try_position =0
+
+// Structre grid div into a 2D array
+const getGrid = ()=>{
+    let grid =[[],[],[],[],[],[]]
+    counter =0;
+    rows =0;
+    for (let index = 0; index < gridFromHTML.length; index++) {
+       for(i=0; i<MAX_NUMBER_OF_LETTERS;i++){
+            if((i+counter)>29){
+                // continue
+                return grid
+            }
+         grid[rows][i] = gridFromHTML[i+counter]
+       }
+       counter =counter+5;
+       rows ++;
+    }
+    return grid
+}
 
 // Use mouse to click the virtual keybaord
 keyboardClicked.forEach(key => {
@@ -21,34 +42,57 @@ keyboardPressed.addEventListener('keydown', e=>{
 
 // Grid Entry Logic
 const gridEntry = (entry) =>{
+    
     // Check if character/letter is valid
     if(/^[a-zA-Z]$/. test(entry)){ 
-        addLetters(entry.toUpperCase(),letter_counter)
-        letter_counter++
-    }// At enter: i)Check if word is 5 letters ii) submit word
+        addLetters(try_position,letter_postion,entry.toUpperCase())
+        letter_postion++
+    }
+    // At enter: i)Check if word is 5 letters ii) submit word
     else if ( entry==='Enter') {
-        if(letter_counter === MAX_NUMBER_OF_LETTERS-1){
+        if(letter_postion === MAX_NUMBER_OF_LETTERS && try_position < MAX_NUMBER_OF_TRIES-1){
             //submit answer
-        }else{
+            console.log('submitted')
+            // Go th the next level
+            try_position++
+            letter_postion=0
+        }else if(letter_postion < MAX_NUMBER_OF_LETTERS){
             // alert not enough letters
+            console.log('not enough letters')
+        }else if(try_position >= MAX_NUMBER_OF_TRIES-1){
+            //submit answer
+            // Out of tries
+            console.log('Out of tries')
+        }else{
+            // inavlid
+            console.log('INVALID')
         }
+
     }// At backspace remove a character/letter
     else if(entry ==='Backspace'){
-        if(letter_counter>0){ // rest the letter counter
-            letter_counter--
+        if(letter_postion>0){ // rest the letter counter
+            letter_postion--
         }else{
-            letter_counter=0
+            letter_postion=0
         }
-        removeLetters(letter_counter)
+        removeLetters(try_position,letter_postion)
     }
 }
 
+// **************
+const grid = getGrid()
+
 // Enter letters on the grid
-const addLetters = (letter,current_index) =>{
-    grid[current_index].textContent = letter
+const addLetters = (current_try,current_index,letter) =>{
+    if(current_index+1<=MAX_NUMBER_OF_LETTERS){
+        grid[current_try][current_index].textContent = letter
+    }else{
+       letter_postion=MAX_NUMBER_OF_LETTERS-1
+       return
+    }
 }
 
 // Enter letters from the grid
-const removeLetters = (current_index) =>{
-    grid[current_index].textContent = ''
+const removeLetters = (current_try,current_index) =>{
+    grid[current_try][current_index].textContent = ''
 }
