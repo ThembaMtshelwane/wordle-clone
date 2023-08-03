1d27b7066d0509535836dcba276b24935a9a5175
Themba Mtshelwane <Thembamm3@gmail.com>
2023-08-03 23:59:42 +0200

    Home page- FE-1: Check Answer and Change CellColor

--- src/modules/keyInputs.js
+++ src/modules/keyInputs.js
@@ -2,18 +2,29 @@ const keyboardClicked = document.querySelectorAll('.keyinput') // Allow click to
 const keyboardPressed = document.querySelector('body') // Allow press keys to be activated anywhere on the page
 const gridFromHTML = document.querySelectorAll('.cell') // Get all the cells of the grid
 
+const dictionary = [
+    "aahed","aalii","aapas","aargh","aarti","abaca","abaci","abacs","abaft","abaht","abaka","abamp","aband","abash","abask","abaya","abbas","abbed","abbes","abcee","abeam","abear","abeat","abeer","abele","abeng","abers","abets","abeys","abies","abius","abjad","abjud","abler","ables","ablet","ablow","abmho","abnet","abohm","aboil","aboma","aboon","abord","abore","aborn","abram","abray","abrim","abrin","abris","absey","absit","abuna","abune","abura","aburn","abuts","abuzz","abyes","abysm","acais","acara","acari","accas","accha","accoy","accra",
+]
+let guessedWord = []
+let guessedWord_processed = ''
+const targetWord = dictionary[0] // Use Date object to genetae a word every day
+
+let allMatches =[0,1,2,3,4]
+
 const MAX_NUMBER_OF_LETTERS =5
 const MAX_NUMBER_OF_TRIES =6
 let letter_postion =0 
 let try_position =0
 
+let isGameOver = false
+
 // Structre grid div into a 2D array
 const getGrid = ()=>{
     let grid =[[],[],[],[],[],[]]
-    counter =0;
-    rows =0;
+    let counter =0;
+    let rows =0;
     for (let index = 0; index < gridFromHTML.length; index++) {
-       for(i=0; i<MAX_NUMBER_OF_LETTERS;i++){
+       for(let i=0; i<MAX_NUMBER_OF_LETTERS;i++){
             if((i+counter)>29){
                 // continue
                 return grid
@@ -25,6 +36,8 @@ const getGrid = ()=>{
     }
     return grid
 }
+
+//* * * * * * * Press and Click keybaord inputs * * * * * * * 
 
 // Use mouse to click the virtual keybaord
 keyboardClicked.forEach(key => {
@@ -51,11 +64,34 @@ const gridEntry = (entry) =>{
     // At enter: i)Check if word is 5 letters ii) submit word
     else if ( entry==='Enter') {
         if(letter_postion === MAX_NUMBER_OF_LETTERS && try_position < MAX_NUMBER_OF_TRIES-1){
-            //submit answer
-            console.log('submitted')
-            // Go th the next level
-            try_position++
-            letter_postion=0
+            // check if word exists
+            if(dictionary.includes(guessedWord_processed)){
+                // is correct
+                if(targetWord===guessedWord_processed){
+                  // all green,end game
+                  addColour(try_position,allMatches,'green')
+                  console.log('Correct, you win')
+                  isGameOver = true
+
+                }else if (yellowMatches(guessedWord_processed).status) {
+                    // add yellow
+                    addColour(try_position,yellowMatches(guessedWord_processed).yellowIndices,'yellow')
+                    console.log('turn these yellow ',yellowMatches(guessedWord_processed).yellowIndices)
+                    goToNextRow()
+                } else if (greenMatches(guessedWord_processed).status) {
+                    // add green
+                    addColour(try_position,greenMatches(guessedWord_processed).grayIndices,'green')
+                    console.log('turn these green ',greenMatches(guessedWord_processed).grayIndices)
+                    goToNextRow()
+                } else{
+                    goToNextRow()
+                }
+
+            }else{
+                console.log('word does not exists')
+            }
+
+ 
         }else if(letter_postion < MAX_NUMBER_OF_LETTERS){
             // alert not enough letters
             console.log('not enough letters')
@@ -79,13 +115,17 @@ const gridEntry = (entry) =>{
     }
 }
 
-// **************
+// * * * * * * * * * Manipulate Grid * * * * * * * * * 
+
 const grid = getGrid()
 
 // Enter letters on the grid
 const addLetters = (current_try,current_index,letter) =>{
-    if(current_index+1<=MAX_NUMBER_OF_LETTERS){
+    if(current_index+1<=MAX_NUMBER_OF_LETTERS && !(isGameOver)){
         grid[current_try][current_index].textContent = letter
+        guessedWord.push(letter)
+        guessedWord_processed = guessedWord.join().replaceAll(',','').toLowerCase()
+        console.log(guessedWord_processed)
     }else{
        letter_postion=MAX_NUMBER_OF_LETTERS-1
        return
@@ -94,5 +134,54 @@ const addLetters = (current_try,current_index,letter) =>{
 
 // Enter letters from the grid
 const removeLetters = (current_try,current_index) =>{
-    grid[current_try][current_index].textContent = ''
+    if (!(isGameOver)) {
+        grid[current_try][current_index].textContent = ''
+        guessedWord.pop()
+        guessedWord_processed = guessedWord.join().replaceAll(',','').toLowerCase()
+        console.log(guessedWord_processed)   
+    }else{
+        guessedWord.pop()
+    }
+}
+
+const addColour = (current_try,indexArray,colour) =>{
+    for (let i = 0; i < indexArray.length; i++) {
+        grid[current_try][indexArray[i]].classList.add(`cell-colour-${colour}`);
+    }
+}
+// Try again in next row
+const goToNextRow =()=>{
+    // Go the next row and try again
+    try_position++    
+    letter_postion=0
+    guessedWord =[]
+}
+
+// * * * * * * * * * Matches * * * * * * * * * 
+const yellowMatches = (word)=>{
+    let yellowIndices =[]
+    let status = false
+
+    for (let i = 0; i < MAX_NUMBER_OF_LETTERS; i++) {
+        if(targetWord.includes(word[i])){
+            status = true
+            yellowIndices.push(i)
+        }                
+    }
+
+    return {status, yellowIndices}
+}
+
+const greenMatches = (word)=>{
+    let greenIndices =[]
+    let status = false
+
+    for (let i = 0; i < MAX_NUMBER_OF_LETTERS; i++) {
+        if(targetWord[i]===(word[i])){
+            status = true
+            greenIndices.push(i)
+        }                
+    }
+
+    return {status, grayIndices}
 }
