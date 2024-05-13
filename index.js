6b55f9c3048c75e9d3d674a0777aa64c2e66db22
Themba Mtshelwane <Thembamm3@gmail.com>
2024-05-13 10:38:52 +0200

    enter user input

--- index.js
+++ index.js
@@ -1,6 +1,33 @@
 document.addEventListener('DOMContentLoaded', function () {
   const closeSection = document.querySelector('.close')
   const modalContainer = document.querySelector('.modalContainer')
+  const keys = document.getElementsByTagName('input')
+  const cells = document.getElementsByClassName('cell')
+  let count = 0
+
+  // Add user input to grid
+  function handleInput (value) {
+    if (count < 30) {
+      cells[count].textContent = value
+      count++
+    }
+  }
+
+  // Handle clicks
+  for (let index = 0; index < keys.length; index++) {
+    keys[index].addEventListener('click', function () {
+      handleInput(keys[index].value)
+    })
+  }
+
+  // Handle press
+  function handleKeyDown (e) {
+    const key = e.key.toUpperCase()
+    if (count < 30 && /^[A-Z]$/.test(key)) {
+      handleInput(key)
+    }
+  }
+  document.addEventListener('keydown', handleKeyDown)
 
   closeSection.addEventListener('click', function () {
     modalContainer.classList.add('hideModal')
